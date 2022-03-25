6438ad2ab6a556c9c55f22b3db98be79ab77158f
iuliiach <h46@yandex.ru>
2022-03-25 12:41:39 +0300

    added changes to nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -23,3 +23,40 @@ for i in range(1, n + 1):
     for j in range(1, 10):
         print(i, '+', j, '=', i + j)
     print()
+  
+# Problem 4: Дано натуральное число n. Напишите программу, которая печатает численный треугольник в соответствии с примером:
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+n = int(input())
+counter = 0
+for i in range(1, n + 1):
+    counter = counter + 1
+    for j in range(i):
+        print(counter, end ='')
+    print()
+
+# **Problem 5: Дано нечетное натуральное число nn. Напишите программу, которая печатает равнобедренный звездный треугольник с основанием, равным nn в соответствии с примером:
+
+# *
+# **
+# ***
+# ****
+# ***
+# **
+# *
+
+n = int(input())
+m = int((n + 1) / 2)
+for i in range(1, m + 1):
+    for j in range(i):
+        print('*', end='')
+    print()
+for i in range(m - 1, 0, -1):
+    for j in range(i):
+        print('*', end='')
+    print()  
