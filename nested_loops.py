14bbfd398eb83155a9113fa16de6256e4bc48962
iuliiach <h46@yandex.ru>
2022-03-28 17:12:10 +0300

    added changes to nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -75,3 +75,18 @@ for i in range(n):
         total += 1
         print(total, end =' ')
     print()
+
+# Problem 7: Дано натуральное число n. Напишите программу, которая печатает численный треугольник с высотой равной n, в соответствии с примером:
+
+# 1
+# 121
+# 12321
+# 1234321
+
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end='')
+    for k in range(i-1, 0, -1):
+        print(k, end='')
+    print()    
